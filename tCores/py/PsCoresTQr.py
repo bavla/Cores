564d3de5c21306bfc950b25a1c72a28756e0c889
bavla <vladimir.batagelj@gmail.com>
2019-03-04 01:22:35 +0100

    Add files via upload

--- tCores/py/PsCoresTQr.py
+++ tCores/py/PsCoresTQr.py
@@ -5,24 +5,12 @@ import sys, os, datetime, json
 sys.path = [gdir]+sys.path; os.chdir(wdir)
 from Nets import Network as N
 import TQ
+eps = 1e-5
 def rounD(n, decimals=0):
    multiplier = 10 ** decimals
    return round(n * multiplier) / multiplier
-# fJSON = 'ConnectivityWeighted.json'
-# fJSON = "violenceE.json"
-# fJSON = 'stem.json'
-# fJSON = 'CcCtest.json'
-# fJSON = 'Terror news 50.json'
-# fJSON = 'CcCSN5.json'
-# fJSON = 'CcTest.json'
 fJSON = 'CcNewman.json'
-# S = N.loadNetJSON(fJSON); G = S.pairs2edges()
-G = N.loadNetJSON(fJSON)
-# G.saveNetJSON(file="Terror50E",indent=1)
-# fJSON = 'ConnectivityTest.json'
-# fJSON = 'ExampleB.json'
-# fJSON = 'PathfinderTest.json'
-# G = N.Graph.loadNetJSON(fJSON)
+G = N.loadNetsJSON(fJSON)
 G.delLoops()
 print("Temporal Ps cores in: ",fJSON)
 t1 = datetime.datetime.now()
@@ -42,18 +30,16 @@ while len(D)>0:
    if step % 100 == 1:
       print("{0:3d}. dmin={1:10.4f}   node={2:4d}".format(step,dmin,u))
    cCore = TQ.TQ.complement(Core[u],Tmin,Tmax+1)
-#   core = TQ.TQ.extract(cCore,[d for d in D[u] if d[2] == dmin])
-   core = TQ.TQ.extract(cCore,[d for d in D[u] if d[2] <= dmin])
+   core = TQ.TQ.extract(cCore,[d for d in D[u] if d[2] <= dmin+eps])
    if core!=[]:
-#      Core[u] = TQ.TQ.sum(Core[u],core)
-      Core[u] = [ (s,f,rounD(v,7)) for (s,f,v) in TQ.TQ.sum(Core[u],core) ]
+      Core[u] = TQ.TQ.sum(Core[u],core)
       D[u] = TQ.TQ.cutGE(TQ.TQ.sum(D[u],TQ.TQ.minus(core)),dmin) 
       for link in G.star(u):
          v = G.twin(u,link)
          if not(v in D): continue
          chLink = TQ.TQ.minus(TQ.TQ.extract(core,G.getLink(link,'tq')))
          if chLink==[]: continue
-         diff = TQ.TQ.cutGE(TQ.TQ.sum(D[v],chLink),0)  
+         diff = TQ.TQ.cutGE(TQ.TQ.sum(D[v],chLink),0-eps)  
          D[v] = [ (sd,fd,max(vd,dmin)) for sd,fd,vd in diff ]
          if len(D[v])==0: del D[v]; del Dmin[v]
          else: Dmin[v] = min([e[2] for e in D[v]])
@@ -63,5 +49,8 @@ print("{0:3d}. dmin={1:10.4f}   node={2:4d}".format(step,dmin,u))
 # print("\n-----\nCore =",Core)
 t2 = datetime.datetime.now()
 print("\nfinished: ",t2.ctime(),"\ntime used: ", t2-t1)
-
+C4 = TQ.TQ.TQdictCut(Core,4-eps)
+CR = { k : TQ.TQ.standard([ (s,f,rounD(v,3)) for (s,f,v) in C4[k] ]) for k in C4 }
+for v in CR:
+   print("{0:5d} : {1:11s} ".format(v,G.getNode(v,'lab')),CR[v])
 
